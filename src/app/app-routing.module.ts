e52d1b687ca52d3bbc752d93a1fcab479f031fda
uday chindanuru <ajaykumarc003@gmail.com>
2022-01-06 21:29:03 +0530

    my commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { BlogComponent } from './blog/blog.component';
+import { CarrersComponent } from './carrers/carrers.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { PressComponent } from './press/press.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contactus', component: ContactusComponent },
+  {path:'press',component:PressComponent},
+  {path:'blog',component:BlogComponent},
+  {path:'careers',component:CarrersComponent},
+  {path:'aboutus',component:AboutusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { CarrersComponent } from './carrers/carrers.component';
+import { PressComponent } from './press/press.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    BlogComponent,
+    ContactusComponent,
+    CarrersComponent,
+    PressComponent
   ],
   imports: [
     BrowserModule,
